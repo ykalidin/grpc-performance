50130da08bda74987d4f90b29a6dc7a54895708e
ykalidin <ykalidin@tibco.com>
2019-05-08 12:15:14 +0530

    Upload Files

---
+++ api/grpc-to-grpc/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"flag"
+
+	"github.com/project-flogo/core/engine"
+	"github.com/ykalidin/grpc-performance/examples"
+	"github.com/ykalidin/grpc-performance/proto/grpc2grpc"
+)
+
+func main() {
+	clientMode := flag.Bool("client", false, "command to run client")
+	serverMode := flag.Bool("server", false, "command to run server")
+	methodName := flag.String("method", "pet", "method name")
+	number := flag.Int("number", 1, "number of times to run client")
+	paramVal := flag.String("param", "user2", "method param")
+	port := flag.String("port", "", "port value")
+	flag.Parse()
+
+	if *clientMode {
+		grpc2grpc.CallClient(port, methodName, *paramVal, nil, *number)
+	} else if *serverMode {
+		server, _ := grpc2grpc.CallServer()
+		server.Start()
+		return
+	} else {
+
+		e, err := examples.GRPC2GRPCExample()
+		if err != nil {
+			panic(err)
+		}
+		engine.RunEngine(e)
+	}
+}
